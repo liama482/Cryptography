f4a4356f1595142213aa29674cde18f0d5a004bc
liama482 <liamabbate@hanovernorwichschools.org>
2016-10-11 14:01:33 -0400

    Updated from Brython Server: 10/11/2016 2:01:33 PM

--- cryptography.py
+++ cryptography.py
@@ -11,13 +11,22 @@ See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptog
 """
 associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
 instr=(input("Enter e to encrypt, d to decrypt, or q to quit: "))
-if instr!="q":
-    msg=input("Message: ")
-    key=input("Key: ")
+lett=int(len(associations)) #computes number of characters in associations
+print(lett)
+t=1
+while t=2:
     if instr!="q":
-else:
-    print("Goodbye!")
-
-associations.find(char)
-associations[index])
-
+        msg=input("Message: ")
+        key=input("Key: ")
+        code = [] #list of encoded individual characters
+        code2 = [] #encoded key
+        for char in range(0,lett):
+            nm=msg.count(associations[char]) #finds the number of times a letter is in the string
+            if nm!=0: #if the letter's in the string...
+                occurr=associations.find(associations[char])
+                code.append(occurr)
+        print(list(code))
+    else:
+        print("Goodbye!")
+        t=1
+#associations[index]
