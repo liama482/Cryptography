3a001acd97349ae6c54414813345e39f4277e126
liama482 <liamabbate@hanovernorwichschools.org>
2016-10-14 13:40:31 -0400

    Updated from Brython Server: 10/14/2016 1:40:31 PM

--- cryptography.py
+++ cryptography.py
@@ -1,7 +1,7 @@
 """
 cryptography.py
 Author: Liam
-Credit: ??
+Credit: none
 
 Assignment:
 
@@ -10,23 +10,36 @@ Write and submit a program that encrypts and decrypts user data.
 See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptography/blob/master/README.md
 """
 associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
-instr=(input("Enter e to encrypt, d to decrypt, or q to quit: "))
 lett=int(len(associations)) #computes number of characters in associations
-print(lett)
+#print(lett)
+import time
+
+instr=(input("Enter e to encrypt, d to decrypt, or q to quit: "))
 t=2
 while t==2:
     if instr!="q":
         msg=input("Message: ")
         key=input("Key: ")
-        code = [] #list of encoded individual characters
+        code = [] #(empty) list of encoded individual characters
         code2 = [] #encoded key
-        for char in range(0,lett):
-            nm=msg.count(associations[char]) #finds the number of times a letter is in the string
-            if nm!=0: #if the letter's in the string...
-                occurr=associations.find(associations[char])
-                code.append(occurr)
+        wo=int(len(msg)) #computes number of characters in message
+        ko=int(len(key)) #computes number of characters in key
+        for char in range(0,wo):
+            #nm=msg.count(associations[char]) #finds the number of times a letter is in the string
+            #if nm!=0: #if the letter's in the string...
+            occurr=associations.find(msg[char])
+            code.append(occurr)
         print(list(code))
+        """
+        for char in range(0,wo):
+            nma=key.count(associations[char]) #finds the number of times a letter is in the string
+            if nma!=0: #if the letter's in the string...
+                occurred=associations.find(associations[char])
+                code2.append(occurred)
+        """
+        print(list(code2))
     else:
         print("Goodbye!")
-        t=1
+    t=1
+    #time.sleep(3) # delays for 3 seconds
 #associations[index]
